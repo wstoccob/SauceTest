a4eb8539e5755793831151db6624ccedfc072bd4
Alinur Seisekov <alinur.seysekov@gmail.com>
2025-03-14 19:09:44 +0500

    Implement UC1

---
+++ SauceTest/Pages/IndexPage.cs
@@ -0,0 +1,53 @@
+﻿using OpenQA.Selenium;
+
+namespace SauceTest.Pages;
+
+public class IndexPage
+{
+    private static string Url { get; } = "https://www.saucedemo.com/";
+    private readonly WebDriver driver;
+
+    public IndexPage(WebDriver driver) => this.driver = driver ?? throw new ArgumentNullException(nameof(driver));
+
+    private IWebElement LoginInput => driver.FindElement(By.Id("user-name"));
+    private IWebElement PasswordInput => driver.FindElement(By.Id("password"));
+    private IWebElement SubmitButton => driver.FindElement(By.Id("login-button"));
+    
+    private string ControlCommand
+    {
+        get
+        {
+            string? platform = driver.Capabilities.GetCapability("platformName") as string;
+            ArgumentNullException.ThrowIfNull(platform);
+            return platform.Contains("mac") ? Keys.Command : Keys.Control;
+        }
+    }
+
+    public IndexPage Open()
+    {
+        driver.Url = Url;
+        return this;
+    }
+
+    public void TypeLoginCredentials(string loginCredentials) => LoginInput.SendKeys(loginCredentials);
+
+    public void TypePasswordCredentials(string passwordCredentials) => PasswordInput.SendKeys(passwordCredentials);
+
+    public void ClearLoginInput()
+    {
+        LoginInput.SendKeys(ControlCommand + "a" + Keys.Delete);
+    }
+
+    public void ClearPasswordInput()
+    {
+        PasswordInput.SendKeys(ControlCommand + "a" + Keys.Delete);
+    }
+
+    public void SubmitForm() => SubmitButton.Click();
+
+    public string GetError()
+    {
+        var errorElement = driver.FindElement(By.TagName("h3"));
+        return errorElement.Text;
+    }
+}
--- SauceTest/UnitTest1.cs
+++ SauceTest/UnitTest1.cs
@@ -1,15 +1,40 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using SauceTest.Pages;
+
 namespace SauceTest;
 
+[Parallelizable]
 public class Tests
 {
+    private WebDriver driver;
     [SetUp]
     public void Setup()
     {
+        driver = new ChromeDriver();
     }
 
     [Test]
-    public void Test1()
+    public void UC1_TestLoginForm_EmptyCredentials()
     {
-        Assert.Pass();
+        const string RANDOM_LOGIN_CREDENTIALS = "123";
+        const string RANDOM_PASSWORD_CREDENTIALS = "123";
+        const string EXPECTED_ERROR = "Epic sadface: Username is required";
+        
+        var page = new IndexPage(driver);
+        page.Open();
+        page.TypeLoginCredentials(RANDOM_LOGIN_CREDENTIALS);
+        page.TypePasswordCredentials(RANDOM_PASSWORD_CREDENTIALS);
+        page.ClearLoginInput();
+        page.ClearPasswordInput();
+        page.SubmitForm();
+        var actualError = page.GetError();
+        Assert.That(actualError, Is.EqualTo(EXPECTED_ERROR));
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        driver.Dispose();
     }
 }
